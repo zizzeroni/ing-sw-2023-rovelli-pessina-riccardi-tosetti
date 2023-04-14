3d5fe4397f43a81d608a75d0b1840ca37570822f
Francesco Riccardi <francesco4.riccardi@mail.polimi.it>
2023-04-14 10:44:47 +0200

    Creazione della base della comunicazione socket client-server Creata classe EchoClientSocket con lettura e scrittura su stream, classe MainServer che avvia il server generale EchoServerSocket, questa classe riceve connsessioni da client e li accoppia con un SingleClientHandler che li gestisce singolarmente, nella classe EchoServerSocket è mantenuta una lista interna con gli accoppiamenti tra client e handler

---
+++ src/main/java/it/polimi/ingsw/network/EchoServerSocket.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.network;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EchoServerSocket extends Thread {
+    private final int port = 1337;
+    private ServerSocket serverSocket;
+    private List<SingleClientHandler> clientHandlers;
+    //Questa è una lista di client che verranno passati al server MultiClient che li gestirà singolarmente
+
+    public EchoServerSocket() throws IOException {
+        clientHandlers = new ArrayList<>();
+
+        serverSocket = new ServerSocket(port);
+
+        this.start();
+    }
+    public void closeConnection() throws IOException {
+        serverSocket.close();
+    }
+    public void run() {
+        //aggiunge un handler sulla connnessione appena arrivata e lo fa partire
+        while(true) {
+            try {
+                clientHandlers.add(new SingleClientHandler(serverSocket.accept()));
+                clientHandlers.get(clientHandlers.size()-1).start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
