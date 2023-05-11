7388d7ec4f08785f9b8db806e82889c69b091245
Francesco Riccardi <francesco4.riccardi@mail.polimi.it>
2023-05-11 12:01:33 +0200

    Prime implementazioni javaFX Aggiunte tutte le immagini Aggiunte le prime due schermate Aggiunto il Main della Gui ed il controller della stessa Aggiunta una classe css funzionante al 50%

---
+++ src/main/java/it/polimi/ingsw/GUI/MainGui.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainGui extends Application{
+    @Override
+    public void start(Stage primaryStage)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/FirstScene.fxml"));
+        primaryStage.setTitle("First Scene");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
