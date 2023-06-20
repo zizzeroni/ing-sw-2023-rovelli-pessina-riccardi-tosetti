5afb8022c9d370c9d08f86c7967857af7d37af4c
Alessandro Pessina <alessandro3.pessina@mail.polimi.it>
2023-06-20 17:02:29 +0200

    added tile javadoc

--- src/main/java/it/polimi/ingsw/model/tile/ScoreTile.java
+++ src/main/java/it/polimi/ingsw/model/tile/ScoreTile.java
@@ -1,48 +1,106 @@
 package it.polimi.ingsw.model.tile;
 
+/**
+ * This class represents a {@code ScoreTile}.
+ * Provides a series of methods to access and modify the ScoreTile's value
+ * and the linked {@code Player}'s and {@code CommonGoal}'s identifiers.
+ *
+ * @see it.polimi.ingsw.model.Player
+ * @see it.polimi.ingsw.model.commongoal.CommonGoal
+ */
 public class ScoreTile {
     private int value;
     private int playerID; // reference to the player that owns the scoreTile
     private int commonGoalID; // reference to the common goal the scoreTile refers to
 
+    /**
+     * Builder of the {@code ScoreTile}'s class.
+     * Constructs a ScoreTile considering its assigned value
+     * and also current player's and common goal's id.
+     *
+     * @param value the value associated to the given ScoreTile.
+     * @param playerID
+     * @param commonGoalID
+     */
     public ScoreTile(int value, int playerID, int commonGoalID) {
         this.value = value;
         this.playerID = playerID;
         this.commonGoalID = commonGoalID;
     }
 
+    /**
+     * Builder of the {@code ScoreTile}'s class.
+     * Constructs a ScoreTile.
+     */
     public ScoreTile() {
         this.value = -1;
         this.playerID = -1;
         this.commonGoalID = -1;
     }
 
+    /**
+     * Builder of the {@code ScoreTile}'s class.
+     * Constructs a ScoreTile considering its assigned value.
+     *
+     * @param value the value associated to the given ScoreTile.
+     */
     public ScoreTile(int value) {
         this.value = value;
         this.playerID = -1;
         this.commonGoalID = -1;
     }
 
+    /**
+     * Sets the {@code ScoreTile}'s value.
+     *
+     * @param value is the value assigned to the ScoreTile.
+     *
+     */
     public void setValue(int value) {
         this.value = value;
     }
 
+    /**
+     * Getter to retrieve the {@code Player}'s identifier.
+     *
+     * @return the {@code Player}'s identifier.
+     */
     public int getPlayerID() {
         return this.playerID;
     }
 
+    /**
+     * Sets the {@code Player}'s identifier.
+     *
+     * @param playerID is the reference to the player that owns the scoreTile.
+     */
     public void setPlayerID(int playerID) {
         this.playerID = playerID;
     }
 
+    /**
+     * Getter to retrieve the {@code CommonGoal}'s identifier.
+     *
+     * @return reference to the common goal the scoreTile refers to.
+     */
     public int getCommonGoalID() {
         return this.commonGoalID;
     }
 
+    /**
+     * Setter to assign the {@code CommonGoal}'s identifier.
+     *
+     * @return the reference to the common goal the scoreTile refers to.
+     */
     public void setCommonGoalID(int commonGoalID) {
         this.commonGoalID = commonGoalID;
     }
 
+    /**
+     * Sets the {@code ScoreTile}'s value.
+     *
+     * @return the value assigned to the ScoreTile.
+     */
     public int getValue() {
         return this.value;
     }
--- src/main/java/it/polimi/ingsw/model/tile/Tile.java
+++ src/main/java/it/polimi/ingsw/model/tile/Tile.java
@@ -1,35 +1,87 @@
 package it.polimi.ingsw.model.tile;
 
+/**
+ * This class represents the {@code Tile} and all the necessary
+ * methods (getters and setters) used to access and modify its fields (color, imageID).
+ */
 public class Tile {
     private TileColor color;
     private int imageID;
 
+    /**
+     * Constructor of the class, sets tile color and id to default values.
+     */
     public Tile() {
         this.color = null;
         this.imageID = 0;
     }
 
+    /**
+     * Constructor of the class, assigns {@code Tile}'s
+     * color and id (passed as parameters).
+     *
+     * @param color the tile's color.
+     * @param imageID the identifier associated to the tile.
+     *
+     * @see Tile
+     */
     public Tile(TileColor color, int imageID) {
         this.color = color;
         this.imageID = imageID;
     }
 
+    /**
+     * Getter used to access {@code Tile}'s image identifier.
+     *
+     * @return the tile's imageID.
+     *
+     * @see Tile
+     */
     public int getImageID() {
         return this.imageID;
     }
 
+    /**
+     * Setter used to modify {@code Tile}'s imageID.
+     *
+     * @param imageID is the image identifier of the considered tile.
+     *
+     * @see Tile
+     */
     public void setImageID(int imageID) {
         this.imageID = imageID;
     }
 
+    /**
+     * Class constructor.
+     * Sets the {@code Tile}'s color to an initial value.
+     *
+     * @param color is the color given to the current tile.
+     *
+     * @see Tile
+     */
     public Tile(TileColor color) {
         this.color = color;
     }
 
+    /**
+     * Getter used to access {@code Tile}'s color.
+     *
+     * @return the tile's color.
+     *
+     * @see Tile
+     */
     public TileColor getColor() {
         return this.color;
     }
 
+    /**
+     * Setter used to modify {@code Tile}'s color.
+     *
+     * @param color is the color of the considered tile.
+     *
+     * @see Tile
+     */
     public void setColor(TileColor color) {
         this.color = color;
     }
