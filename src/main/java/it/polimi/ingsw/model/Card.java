12396ddf3ccd72891eab3ebeb0f752910ac6c0ec
Alessandro Pessina <alessandro3.pessina@mail.polimi.it>
2023-06-22 21:21:55 +0200

    Added javadoc modifies

--- src/main/java/it/polimi/ingsw/model/Card.java
+++ src/main/java/it/polimi/ingsw/model/Card.java
@@ -1,18 +1,33 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.commongoal.CommonGoal;
+import it.polimi.ingsw.model.tile.Tile;
+
 /**
+ * This class is used to represent the Card objects.
+ * It provides methods to access and modify the image of the card, plus a method
+ * to associate the card to the pattern it represents and counting the number of its
+ * repetitions inside the {@code Bookshelf}.
  *
+ * @see Bookshelf
  */
 public abstract class Card {
     private int imageID;
 
     /**
-     *
+     * Class constructor.
+     * Initialize the single card.
      */
     public Card() {
         this.imageID = 0;
     }
 
+    /**
+     * Class constructor.
+     * Initialize the card's image.
+     *
+     * @param imageID the card's image.
+     */
     public Card(int imageID) {
         this.imageID = imageID;
     }
@@ -23,18 +38,24 @@ public abstract class Card {
      *
      * @param bookshelf is the selected {@code Bookshelf}.
      * @return the total number of goal pattern repetitions.
+     *
+     * @see Bookshelf
      */
     public abstract int numberOfPatternRepetitionInBookshelf(Bookshelf bookshelf);
 
     /**
-     * @return
+     * Getter used to access {@code Card}'s image identifier.
+     *
+     * @return the {@code Card}'s imageID.
      */
     public int getImageID() {
         return this.imageID;
     }
 
     /**
-     * @param imageID
+     * Setter used to modify {@code Card}'s imageID.
+     *
+     * @param imageID is the image identifier of the considered card.
      */
     public void setImageID(int imageID) {
         this.imageID = imageID;
