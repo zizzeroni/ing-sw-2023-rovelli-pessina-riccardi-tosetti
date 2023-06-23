51671b4d96fd343a8d167c9af8f96154dfadea16
Alessandro Pessina <alessandro3.pessina@mail.polimi.it>
2023-06-23 18:36:59 +0200

    added javadoc modifies

--- src/main/java/it/polimi/ingsw/network/socketMiddleware/commandPatternClientToServer/SendPrivateMessageCommand.java
+++ src/main/java/it/polimi/ingsw/network/socketMiddleware/commandPatternClientToServer/SendPrivateMessageCommand.java
@@ -5,26 +5,61 @@ import it.polimi.ingsw.network.socketMiddleware.CommandType;
 
 import java.rmi.RemoteException;
 
+/**
+ * This class represents the command of the private message transmission.
+ * It contains a series of methods to access and modify the class attributes (getters and setters)
+ * and for the associated command execution and displaying.
+ * It is developed as an implementation of the {@code CommandToServer} interface.
+ *
+ *
+ * @see CommandToServer
+ */
 public class SendPrivateMessageCommand implements CommandToServer {
     private Server actuator;
     private String receiver;
     private String sender;
     private String content;
 
+    /**
+     * Class constructor.
+     */
     public SendPrivateMessageCommand() {
         this.actuator = null;
     }
 
+    /**
+     * Class constructor.
+     * Initialize the content, sender and receiver of the private message command.
+     *
+     * @param content the content of the message.
+     * @param sender the sender of the message.
+     * @param receiver the receiver of the message.
+     */
     public SendPrivateMessageCommand(String receiver, String sender, String content) {
         this.receiver = receiver;
         this.sender = sender;
         this.content = content;
     }
 
+    /**
+     * Class constructor.
+     * Initialize the actuator to the given value.
+     *
+     * @param actuator the command's actuator.
+     */
     public SendPrivateMessageCommand(Server actuator) {
         this.actuator = actuator;
     }
 
+    /**
+     * Class constructor.
+     * Initialize the actuator, content, sender and receiver of the private message command.
+     *
+     * @param actuator the command's actuator.
+     * @param content the content of the message.
+     * @param sender the sender of the message.
+     * @param receiver the receiver of the message.
+     */
     public SendPrivateMessageCommand(Server actuator, String receiver, String sender, String content) {
         this.actuator = actuator;
         this.receiver = receiver;
@@ -32,16 +67,32 @@ public class SendPrivateMessageCommand implements CommandToServer {
         this.content = content;
     }
 
+    /**
+     * Gets the change turn command's actuator.
+     *
+     * @return the actuator of the private message sending command.
+     */
     @Override
     public Server getActuator() {
         return this.actuator;
     }
 
+    /**
+     * Sets the change turn command's actuator.
+     *
+     * @param actuator the actuator of the private message sending command.
+     */
     @Override
     public void setActuator(Server actuator) {
         this.actuator = actuator;
     }
 
+    /**
+     * This method permits the execution of the private message sending command.
+     *
+     * @throws NullPointerException if there is no command to execute.
+     * @throws RemoteException
+     */
     @Override
     public void execute() throws NullPointerException, RemoteException {
         if (this.actuator != null) {
@@ -51,11 +102,25 @@ public class SendPrivateMessageCommand implements CommandToServer {
         }
     }
 
+    /**
+     * Used to enumerate the type of the class command.
+     *
+     * @return the {@code CommandType} of the private message sending command.
+     *
+     * @see CommandType
+     */
     @Override
     public CommandType toEnum() {
         return CommandType.SEND_PRIVATE_MESSAGE;
     }
 
+    /**
+     * Displays the type of command being executed altogether with the command receiver ({@code GameController}) and command parameters.
+     *
+     * @return the string representing the class command.
+     *
+     * @see it.polimi.ingsw.controller.GameController
+     */
     @Override
     public String toString() {
         return "[CommandReceiver:GameController, CommandType:SendPrivateMessage, Parameters:{Receiver:" + this.receiver + "; Sender:" + this.sender + "; Content:" + this.content + "}]";
