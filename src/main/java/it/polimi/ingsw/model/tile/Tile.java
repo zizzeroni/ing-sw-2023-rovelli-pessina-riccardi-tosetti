e26b6d2d257da2a2c6d05b798d1a762066846985
Andrea Rovelli <andrea4.rovelli@mail.polimi.it>
2023-06-26 23:11:06 +0200

    Ninth commit aligned

--- src/main/java/it/polimi/ingsw/model/tile/Tile.java
+++ src/main/java/it/polimi/ingsw/model/tile/Tile.java
@@ -1,35 +1,87 @@
 package it.polimi.ingsw.model.tile;
 
+/**
+ * This class represents the {@code Tile} and all the necessary
+ * methods (getters and setters) used to access and modify its fields (color, id).
+ */
 public class Tile {
     private TileColor color;
     private int id;
 
+    /**
+     * Constructor of the class, sets tile color and id to default values.
+     */
     public Tile() {
         this.color = null;
         this.id = 0;
     }
 
+    /**
+     * Constructor of the class, assigns {@code Tile}'s
+     * color and id (passed as parameters).
+     *
+     * @param color the tile's color.
+     * @param id the identifier associated to the tile.
+     *
+     * @see Tile
+     */
     public Tile(TileColor color, int id) {
         this.color = color;
         this.id = id;
     }
 
+    /**
+     * Getter used to access {@code Tile}'s image identifier.
+     *
+     * @return the tile's id.
+     *
+     * @see Tile
+     */
     public int getId() {
         return this.id;
     }
 
-    public void setid(int id) {
+    /**
+     * Setter used to modify {@code Tile}'s id.
+     *
+     * @param id is the identifier of the considered tile.
+     *
+     * @see Tile
+     */
+    public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Class constructor.
+     * Sets the {@code Tile}'s color to an initial value.
+     *
+     * @param color is the color given to the current tile.
+     *
+     * @see Tile
+     */
     public Tile(TileColor color) {
         this.color = color;
     }
 
+    /**
+     * Getter used to access {@code Tile}'s color.
+     *
+     * @return the tile's color.
+     *
+     * @see Tile
+     */
     public TileColor getColor() {
         return this.color;
     }
 
+    /**
+     * Setter used to modify {@code Tile}'s color.
+     *
+     * @param color is the color of the considered tile.
+     *
+     * @see Tile
+     */
     public void setColor(TileColor color) {
         this.color = color;
     }
--- src/main/java/it/polimi/ingsw/model/tile/TileColor.java
+++ src/main/java/it/polimi/ingsw/model/tile/TileColor.java
@@ -4,9 +4,20 @@ import org.fusesource.jansi.Ansi;
 
 import static org.fusesource.jansi.Ansi.ansi;
 
+/**
+ * Enums all the possible colors that the {@code Tile} can assume
+ * and provides methods to adjust color in CLI and GUI.
+ */
 public enum TileColor {
     GREEN, YELLOW, WHITE, BLUE, CYAN, PURPLE;
 
+    /**
+     * Redefines the toString method to incorporate colored text.
+     *
+     * @return the string used to represent the color associated to the Tile.
+     *
+     * @see Tile
+     */
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
@@ -21,6 +32,11 @@ public enum TileColor {
         return stringBuilder.append(ansi().fg(color).a(this.name().charAt(0)).fg(Ansi.Color.DEFAULT)).toString();
     }
 
+    /**
+     * Defines a string associated to the colored text for the GUI's representation.
+     *
+     * @return the string used to represent the color text associated to the Tile.
+     */
     public String toGUI() {
         return String.valueOf(this.name().charAt(0));
     }
