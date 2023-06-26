1b7d0a3ce8129c3e387a93830b6275f1b795d9b9
Andrea Rovelli <andrea4.rovelli@mail.polimi.it>
2023-06-26 23:07:42 +0200

    Eighth commit aligned

--- src/main/java/it/polimi/ingsw/model/listeners/GameListener.java
+++ src/main/java/it/polimi/ingsw/model/listeners/GameListener.java
@@ -1,15 +1,62 @@
 package it.polimi.ingsw.model.listeners;
 
+import it.polimi.ingsw.model.Game;
+
+/**
+ * An interface used to represent an object of type 'listener'.
+ * In this case the listener registers itself to the {@code Game}.
+ * When the view is subject to changes the listener responds to them
+ * through a set of methods to notify the adding of a {@code Player},
+ * if the number of active players has been modified, if the active
+ * player index has been modified, ...
+ * It also detects if the game's {@code Bag} has been modified.
+ *
+ * @see Game
+ */
 public interface GameListener {
+    /**
+     * Notifies if the active {@code numberOfPlayers}
+     * into the {@code Game}'s lobby has been modified.
+     *
+     * @see it.polimi.ingsw.model.Game
+     */
     public void numberOfPlayersModified();
 
+    /**
+     * Notifies if the {@code activePlayerIndex}
+     * has been modified.
+     */
     public void activePlayerIndexModified();
 
+    /**
+     * Notifies if the bag has been modified
+     * trough shuffling its {@code Tile}s
+     *
+     * @see it.polimi.ingsw.model.tile.Tile
+     */
     public void bagModified();
 
+    /**
+     * Notifies if the evaluated {@code commonGoal}
+     * has been modified in the current {@code Game}.
+     *
+     * @see it.polimi.ingsw.model.Game
+     */
     public void commonGoalsModified();
 
+    /**
+     * Notifies if another {@code Player} has entered
+     * into the {@code Game}'s lobby.
+     *
+     * @see it.polimi.ingsw.model.Game
+     */
     public void addedPlayer();
 
+    /**
+     * Notifies if the active {@code Game}'s state
+     * has changed
+     *
+     * @see Game#getGameState()
+     */
     public void gameStateChanged();
 }
--- src/main/java/it/polimi/ingsw/model/listeners/ModelListener.java
+++ src/main/java/it/polimi/ingsw/model/listeners/ModelListener.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.model.listeners;
 
+/**
+ * An interface used to represent an object of type 'listener'.
+ * In this case the listener registers itself to the {@code Model}.
+ * When the view is subject to changes the listener permits the model to respond to them.
+ */
 public interface ModelListener extends GameListener, BookshelfListener, BoardListener, PlayerListener {
     //TODO: Chiedere a rovo se implementare il listener del modello oppure usare i GameListener attraverso il metodo setGameState risettando allo stesso stato precedente il modello
     //public void gameRestored();
