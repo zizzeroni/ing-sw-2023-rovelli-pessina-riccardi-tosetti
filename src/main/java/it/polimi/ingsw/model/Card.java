ab56d3837329bcf25996b42607f2784c09e80d8c
Andrea Rovelli <andrea4.rovelli@mail.polimi.it>
2023-06-26 23:25:29 +0200

    Sixteenth commit aligned;

--- src/main/java/it/polimi/ingsw/model/Card.java
+++ src/main/java/it/polimi/ingsw/model/Card.java
@@ -1,18 +1,33 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.commongoal.CommonGoal;
+import it.polimi.ingsw.model.tile.Tile;
+
 /**
+ * This class is used to represent the Card objects.
+ * It provides methods to access and modify the image of the card, plus a method
+ * to associate the card to the pattern it represents and counting the number of its
+ * repetitions inside the {@code Bookshelf}.
  *
+ * @see Bookshelf
  */
 public abstract class Card {
     private int id;
 
     /**
-     *
+     * Class constructor.
+     * Initialize the single card.
      */
     public Card() {
         this.id = 0;
     }
 
+    /**
+     * Class constructor.
+     * Initialize the card's identifier.
+     *
+     * @param id the card's identifier.
+     */
     public Card(int id) {
         this.id = id;
     }
@@ -23,18 +38,24 @@ public abstract class Card {
      *
      * @param bookshelf is the selected {@code Bookshelf}.
      * @return the total number of goal pattern repetitions.
+     *
+     * @see Bookshelf
      */
     public abstract int numberOfPatternRepetitionInBookshelf(Bookshelf bookshelf);
 
     /**
-     * @return
+     * Getter used to access {@code Card}'s image identifier.
+     *
+     * @return the {@code Card}'s imageID.
      */
     public int getId() {
         return this.id;
     }
 
     /**
-     * @param id
+     * Setter used to modify {@code Card}'s identifier.
+     *
+     * @param id is the identifier of the considered card.
      */
     public void setId(int id) {
         this.id = id;
